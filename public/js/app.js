9f37c185c04ff743bccf99007d36e3e56d2f78f4
Mrityunjai Kushawaha <mrityunjai4u@gmail.com>
2022-10-22 16:47:32 +0530

    Initial Commit

---
+++ public/js/app.js
@@ -0,0 +1,52 @@
+
+console.log('Client side javascript file is loaded!')
+
+
+//Set Up function to return Weather for the Loaction
+function SubmitForm(searchTerm,callback)
+{
+const geocodeURL=`http://localhost:3000/weather?address=${searchTerm}`;
+fetch(geocodeURL)
+.then(response=>response.json().
+then(
+    data=>
+    {
+        if(data.error)
+        return callback(data.error,undefined);
+        return callback(undefined,
+            {
+            forecast:data.forecast,
+            location:data.location
+        })
+        
+    }
+    )
+    );
+}
+    
+    const form=document.querySelector('form');
+    const input=document.querySelector('input');
+    const firstPara=document.querySelector('#message1')
+    const secondPara=document.querySelector('#message2')
+    form.addEventListener('submit',(e)=>{
+        e.preventDefault();
+        firstPara.textContent='Loading...'
+        secondPara.textContent=''
+        SubmitForm(input.value,(error,data)=>{
+
+            if(error)
+            {
+                firstPara.textContent=error;
+                 
+               return;
+            
+            }
+            console.log(data);
+            firstPara.textContent=data.location;
+            secondPara.textContent= data.forecast;
+
+        })
+
+        
+    })
+    
